2a1c702b544d0b3711fceb964822e74f7c05b9b2
Fox-sys <borisberestov@ya.ru>
2023-01-02 17:48:34 +0300

    orders - таблица с заказами

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+import orders.models as models
 
-# Register your models here.
+
+class SubTaskInline(admin.StackedInline):
+    model = models.SubTask
+    extra = 1
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'closed', 'payed', 'deadline', 'status')
+    inlines = (SubTaskInline, )
+    search_fields = ('id', 'title', 'client', 'status')
+
+
+@admin.register(models.Status)
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('__str__', )
